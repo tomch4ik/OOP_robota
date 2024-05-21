3984b85b4d854097569d3cc4bcb59dbb12de4565
Samof_qs56@student.itstep.org <Samof_qs56@student.itstep.org>
2024-05-21 11:39:29 +0300

    calculator

--- OOP_robota/main.cpp
+++ OOP_robota/main.cpp
@@ -1,1 +1,34 @@
 #include <iostream>
+using namespace std;
+
+int plusa(int a, int b)
+{
+	int result = a + b;
+	return result;
+}
+int minusu(int a, int b)
+{
+	int result = a - b;
+	return result;
+}
+int mnozyt(int a, int b)
+{
+	int result = a * b;
+	return result;
+}
+int delit(int a, int b)
+{
+	int result = a / b;
+	return result;
+}
+
+int main()
+{
+	int a, b;
+	cout << "Input number a and number b" << endl;
+	cin >> a >> b;
+	cout << "a + b = " << plusa(a, b) << endl;
+	cout << "a - b = " << minusu(a, b) << endl;
+	cout << "a * b = " << mnozyt(a, b) << endl;
+	cout << "a / b = " << delit(a, b) << endl;
+}
